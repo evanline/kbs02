a09dfc466bedd775f453b5dfb404696a79e2831c
evanline <ietsenniemand@gmail.com>
2016-04-12 12:09:47 +0200

    details.

--- afgehandeld.php
+++ afgehandeld.php
@@ -5,7 +5,7 @@
 	<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<h1 class= "titel">Aanvraag geslaagd</h1>
+<h1 class= "titel">Aanvraag in behandeling</h1>
 <p class = "form">Uw brochure wordt binnen enkele minuten verstuurd. mocht u niets ontvangen, bekijk dan uw spam-folder of probeer het nog een keer.</p>
 </body>
 </html>
