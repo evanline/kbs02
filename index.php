6e91247da2f467779ceec8ffc2310fd6032b5da5
evanline <ietsenniemand@gmail.com>
2016-04-12 10:57:03 +0200

    maken formulier en style

--- index.php
+++ index.php
@@ -32,15 +32,41 @@ email adres
              header("Location:afgehandeld.php");
         } //todo: dit naar mail.
 
+    } else {
+    	$first_name = "";
+    	$last_name = "";
+    	$email = "";
     }
 ?>
 <html>
 <head>
 	<title>brochure aanvragen</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<form>
-		
+	<h1 class='titel'>Vraag uw brochure aan!</h1>
+
+	<form method = 'post' action = '/index.php'>
+		<p class='form'>Naam:</p>
+        <p class="error"><?php
+            if (isset($_POST['submit']) AND isset($error[0])){echo $error[0];} 
+            ?></p>
+        <p class="error"><?php
+            if (isset($_POST['submit']) AND isset($error[1])){echo $error[1];} 
+            ?></p>
+        <input type="text" name="first_name" placeholder="voornaam" value="<?php echo $first_name; ?>"> 
+        <input type="text" name="last_name" placeholder="achternaam" value="<?php echo $last_name; ?>">
+        <br>
+
+        <p class='form'>Email</p>
+        <p class="error"><?php
+            if (isset($_POST['submit']) AND isset($error[2])){echo $error[2];}
+            ?></p>
+    	<input type="email" name="email" placeholder="email-adres" value="<?php echo $email; ?>">
+    	<br>
+
+    	<input class= "form" type="submit" value="verstuur aanvraag" name="submit">
 	</form>
 </body>
 </html>
